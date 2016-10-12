15c38eabf657579117c993a7bbe4f5356e672339
Ryan <ryan@biobright.org>
2016-10-12 15:07:22 -0400

    Add belongs to relation

--- app/models/aioli.js
+++ app/models/aioli.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  bacon: DS.belongsTo('bacon'),
   mustard: DS.attr('boolean'),
   rating: DS.attr('number')
 });
--- app/models/bacon.js
+++ app/models/bacon.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  aiolis: DS.hasMany('aioli', { async: true }),
   crispy: DS.attr('boolean'),
   rating: DS.attr('number')
 });
