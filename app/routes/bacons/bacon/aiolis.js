d712088893687d71eb48a4331d6e446dcdfaed14
Ryan <ryan@biobright.org>
2016-10-11 20:07:02 -0400

    Update routes to use model

--- app/routes/bacons/bacon/aiolis.js
+++ app/routes/bacons/bacon/aiolis.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('bacon', params.bacon_id);
+    //return this.store.findAll('aioli');
   }
 });
