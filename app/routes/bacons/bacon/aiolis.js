e8eff6c0009dfa301c730849b6c1f11e0b63ac00
Ryan <ryan@biobright.org>
2016-10-12 15:56:00 -0400

    Update ember routes for aioli

--- app/routes/bacons/bacon/aiolis.js
+++ app/routes/bacons/bacon/aiolis.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findAll('aioli');
+    let bacon = this.modelFor("bacons.bacon");
+    return this.store.query('aioli', { bacon_id: bacon.id });
+    //return this.store.findAll('aioli');
   }
 });
